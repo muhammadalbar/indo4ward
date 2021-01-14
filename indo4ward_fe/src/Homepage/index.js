2497436721bcd56a972349c43baca0c032b5dc75
Muhammad Albar <muhammadalbr@gmail.com>
2021-01-14 15:00:48 +0700

    create weather section

--- indo4ward_fe/src/Homepage/index.js
+++ indo4ward_fe/src/Homepage/index.js
@@ -1,42 +1,124 @@
-import React, { useState } from 'react'
-import { Flex, Wrapper } from './styles'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { Flex, Card, Wrapper } from './styles'
 import {TextField, Button, Paper} from '@material-ui/core';
+import NatureIcon from '@material-ui/icons/Nature';
+import SpeedIcon from '@material-ui/icons/Speed';
 import DeleteIcon from '@material-ui/icons/Delete';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
 
 const Homepage = () => {
+    const baseUrl = 'https://indo4ward-db.herokuapp.com/api'
+
+    const [open, setOpen] = useState(false)
+    const Alert = (props) => {
+        return <MuiAlert elevation={6} variant="filled" {...props} />;
+    }
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        setOpen(false);
+    };
+    
+
+    const [info, setInfo] = useState([])
+    useEffect(() => {
+        axios.get(`${baseUrl}/weather`)
+        .then((res) => {
+            const info = res.data
+            console.log(res)
+            setInfo(info)
+        })
+    }, [])
 
     const [value, setValue] = useState({
         activity: ''
     })
+    const [items, setItems] = useState([])
+    useEffect(() => {
+        axios.get(`${baseUrl}/todo`)
+        .then((res) => {
+            const items = res.data.data
+            console.log(res)
+            setItems(items)
+        })
+    }, [])
+
     const handleChange = (e) => {
         setValue({
             activity: e.target.value
         })
     }
-    const handleSubmit = () => {
+
+    const handleSubmit = async () => {
         if(value.activity === ''){
             alert('Insert your activity first!')
         }
         else{
             console.log(value)
+            const todo = {
+                activity: value.activity
+            }
+            await axios.post(`${baseUrl}/create-todo`, todo)
+            .then(res => {
+                console.log(res)
+                console.log(res.data)
+            })
             setValue({
                 activity: ''
             })
+            setOpen(true)
+            setTimeout(function(){ 
+                window.location.reload() 
+            }, 
+                1000);
         }
 
     }
-    const handleDelete = () => {
-        alert('Delete function is not ready yet')
+
+    const handleDelete = (id) => {
+        axios.delete(`${baseUrl}/delete-todo/${id}`)
+        .then((res) => {
+            console.log(res)
+            alert("Your activity has successfully deleted")
+            window.location.reload()
+        })
     }
 
     return(
         <Wrapper>
             <Flex direction="column" alignItems="center">
+                <h1>4 Day Weather Outlook</h1>
+                {info.map((info) => (
+                    <Card>
+                        <Flex direction="column" alignItems="center">
+                            <Flex diretion="row" justify="space-around" wrap="wrap">
+                                <h4>{info.day}</h4>
+                                <h4>{info.forecast}</h4>
+                            </Flex>
+                            <Flex diretion="row" justify="space-around">
+                                <Flex direction="row" justify="center">
+                                    <NatureIcon className="icon" />
+                                    <p>{info.temperature}</p>
+                                </Flex>
+                                <Flex direction="row" justify="center">
+                                    <SpeedIcon className="icon" />
+                                    <p>{info.wind_speed}</p>
+                                </Flex>
+                            </Flex>
+                        </Flex>
+                        
+                    </Card>
+                ))}
+            </Flex>
+            <Flex direction="column" alignItems="center">
                 <h1>To Do List</h1>
                 <Flex direction="row" justify="center">
                     <TextField 
                         id="outlined-basic" 
-                        label="Outlined" 
+                        label="What's your activity today?" 
                         variant="outlined" 
                         value={value.activity}
                         onChange={handleChange} />
@@ -49,15 +131,21 @@ const Homepage = () => {
                     </Button>
                 </Flex>
                 <Flex direction="column" alignItems="center">
-                    <Paper className="paper">
-                        <Flex direction="row" justify="space-between">
-                            <p>Belanja ke supermarket</p>
-                            <DeleteIcon className="deleteBtn" onClick={handleDelete} />
-                        </Flex>
-                    </Paper>
-
+                    {items.map((items) => (
+                        <Paper className="paper">
+                            <Flex direction="row" justify="space-between">
+                                <p>{items.activity}</p>
+                                <DeleteIcon className="deleteBtn" onClick={() => handleDelete(items.id_todo)} />
+                            </Flex>
+                        </Paper>
+                    ))}
                 </Flex>
             </Flex>
+            <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+                <Alert onClose={handleClose} severity="success">
+                    Your activity successfully added!
+                </Alert>
+            </Snackbar>
         </Wrapper>
     )
 }
--- indo4ward_fe/src/Homepage/styles.js
+++ indo4ward_fe/src/Homepage/styles.js
@@ -10,8 +10,14 @@ export const Flex = styled.div`
     height: 100%;
 `
 
+export const Navbar = styled.div`
+    margin-top: 10px;
+    float:right;
+`
+
 export const Wrapper = styled.div`
-    margin-top: 100px;
+    margin-top: 30px;
+    margin-bottom: 100px;
     input{
         width: 20em;
     }
@@ -26,9 +32,38 @@ export const Wrapper = styled.div`
         :hover{
             box-shadow: 0px 4px 4px rgba(1.25, 0, 1.25, 0.25);
         }
+      
     }
     .deleteBtn{
         margin-top: 10px;
         cursor: pointer;
+    }
+    @media(max-width: 414px){
+        input{
+            width: 15em;
+        }
+        .paper{
+            width: 20em;
+        }
+        
+    }
+`
+
+export const Card = styled.div`
+    width: 30em;
+    height: 7.5em;
+    background: #284089;
+    border-bottom: 1px solid white;
+    color: white;
+    .icon{
+        margin-top: 10px;
+        padding-right: 5px;
+    }
+    @media(max-width: 414px){
+        width: 25em;
+        height: auto;
+    }
+    @media(max-width: 375px){
+        width: 21em;
     }
 `
